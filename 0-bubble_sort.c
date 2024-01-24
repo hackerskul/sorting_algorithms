995dbb81a2fcb28e1ecb94b8913a60908e571289
hackerskul <hackerskull254@gmail.com>
2024-01-24 04:38:19 -0500

    c bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, temp,
+
+	if(size < 2):
+		return;
+	for(i = 0; i < size; i++)
+	{
+		for(j = 0; j < size; j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
